8f476315cce5e88a44583589193241df68a33099
aicherishKong <aicherish.kong@gmail.com>
2025-04-14 23:07:58 +0800

    feat: 树结构拖拽

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { MultipleContainers } from "./components/MultipleContainers"
+import { MultipleContainers } from "./components/MultipleContainers";
+import { SortableTree } from "./components/Tree/SortableTree";
 
 function App() {
-
   return (
     <>
-      <MultipleContainers itemCount={3} vertical />
+      {/* <MultipleContainers itemCount={3} vertical /> */}
+      <SortableTree collapsible indicator removable />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
